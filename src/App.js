326105a65b23ba9416c6df25882b1cb97ac2b037
Bex B <bexb1@hotmail.com>
2020-06-23 17:52:43 +0100

    Placeholder landing page finished.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+import Fact from "./Fact";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className=" jumbotron">
+        <div className="jumbotron">
           <h1>Thanks for visiting</h1>
           <p>
             I'm currently in the process of rebuilding this site from scratch.
@@ -15,6 +15,14 @@ function App() {
           <div className="signature">
             <p>- Bex</p>
             <em>23 June 2020</em>
+          </div>
+          <div className="footer">
+            <a href="https://www.linkedin.com/in/bexbolton">
+              <button className="cta">
+                Check out my LinkedIn profile instead
+              </button>
+            </a>
+            <Fact />
           </div>
         </div>
       </header>
