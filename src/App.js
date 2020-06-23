0a954d6a4e08281b62977cdf446e68dbdafab016
Bex B <bexb1@hotmail.com>
2020-06-23 16:49:41 +0100

    Landing page updated with placeholder.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Coming soon!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className=" jumbotron">
+          <h1>Thanks for visiting</h1>
+          <p>
+            I'm currently in the process of rebuilding this site from scratch.
+          </p>
+          <p>Please check back soon!</p>
+          <div className="signature">
+            <p>- Bex</p>
+            <em>23 June 2020</em>
+          </div>
+        </div>
       </header>
     </div>
   );
